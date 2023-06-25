e9cf1682cdb959f3b086fe3a64804423fae19176
hujiebin <iamhujiebin@users.noreply.github.com>
2023-06-25 14:26:30 +0800

    1

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"frozen-go-proxy/rpc"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	r := InitRouter()               // 注册路由
+	r.Run(fmt.Sprintf(":%d", 7003)) // 启动服务
+}
+
+func InitRouter() *gin.Engine {
+	var r = gin.Default()
+	r.Any("/*any", rpc.ReverseProxy)
+	return r
+}
